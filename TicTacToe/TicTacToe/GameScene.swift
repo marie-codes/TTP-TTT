6718e4899932e9b665438d881b68b0bd7cd14d88
Marie J Park <mariejinpark@gmail.com>
2017-04-25 00:05:51 -0400

    TicTacToe Game with AI
    completed using Sprite Kit and GamePlay Kit tutorial

---
+++ TicTacToe/TicTacToe/GameScene.swift
@@ -0,0 +1,96 @@
+//
+//  GameScene.swift
+//  TicTacToe
+//
+//  Created by Marie Park on 4/24/17.
+//  Copyright © 2017 Marie Park. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var gameBoard: Board!
+    var stateMachine: GKStateMachine!
+    var ai: GKMinmaxStrategist!
+    
+    override func didMove(to view: SKView) {
+        
+        self.enumerateChildNodes(withName: "//grid*") { (node, stop) in
+            if let node = node as? SKSpriteNode{
+                node.color = UIColor.clear
+            }
+        }
+        
+        let top_left: BoardCell  = BoardCell(value: .none, node: "//*top_left")
+        let top_middle: BoardCell = BoardCell(value: .none, node: "//*top_middle")
+        let top_right: BoardCell = BoardCell(value: .none, node: "//*top_right")
+        let middle_left: BoardCell = BoardCell(value: .none, node: "//*middle_left")
+        let center: BoardCell = BoardCell(value: .none, node: "//*center")
+        let middle_right: BoardCell = BoardCell(value: .none, node: "//*middle_right")
+        let bottom_left: BoardCell = BoardCell(value: .none, node: "//*bottom_left")
+        let bottom_middle: BoardCell = BoardCell(value: .none, node: "//*bottom_middle")
+        let bottom_right: BoardCell = BoardCell(value: .none, node: "//*bottom_right")
+        
+        let board = [top_left, top_middle, top_right, middle_left, center, middle_right, bottom_left, bottom_middle, bottom_right]
+        
+        gameBoard = Board(gameboard: board)
+        
+        ai = GKMinmaxStrategist()
+        ai.maxLookAheadDepth = 9
+        ai.randomSource = GKARC4RandomSource()
+        
+        let beginGameState = StartGameState(scene: self)
+        let activeGameState = ActiveGameState(scene: self)
+        let endGameState = EndGameState(scene: self)
+        
+        stateMachine = GKStateMachine(states: [beginGameState, activeGameState, endGameState])
+        stateMachine.enter(StartGameState.self)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            let selectedNode = self.atPoint(location)
+            var node: SKSpriteNode
+            
+            if let name = selectedNode.name {
+                if name == "Reset" || name == "reset_label"{
+                    self.stateMachine.enter(StartGameState.self)
+                    return
+                }
+            }
+            
+            if gameBoard.isPlayerOne(){
+                let cross = SKSpriteNode(imageNamed: "X_symbol")
+                cross.size = CGSize(width: 75, height: 75)
+                cross.zRotation = CGFloat(M_PI / 4.0)
+                node = cross
+            }
+            else{
+                let circle = SKSpriteNode(imageNamed: "O_symbol")
+                circle.size = CGSize(width: 75, height: 75)
+                node = circle
+            }
+            
+            for i in 0...8{
+                guard let cellNode: SKSpriteNode = self.childNode(withName: gameBoard.getElementAtBoardLocation(i).node) as? SKSpriteNode else{
+                    return
+                }
+                if selectedNode.name == cellNode.name{
+                    cellNode.addChild(node)
+                    gameBoard.addPlayerValueAtBoardLocation(i, value: gameBoard.isPlayerOne() ? .x : .o)
+                    gameBoard.togglePlayer()
+                }
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before each frame is rendered */
+        self.stateMachine.update(deltaTime: currentTime)
+    }
+}
---
+++ TicTacToe/TicTacToe/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  TicTacToe
+//
+//  Created by Marie Park on 4/24/17.
+//  Copyright © 2017 Marie Park. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    override var shouldAutorotate : Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .portrait
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
